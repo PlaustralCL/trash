620433c49cd34eb4a204e06d349441363fa00d78
PlaustralCL <plaustralcl@gmail.com>
2026-03-15 11:42:28 -0400

    Create separate function for finding HOME

--- trash.go
+++ trash.go
@@ -2,6 +2,7 @@ package main
 
 import (
     "fmt"
+    "log"
     "os"
 )
 
@@ -9,12 +10,14 @@ import (
 // Trash location = $XDG_DATA_HOME/Trash
 
 func main() {
-    home, ok := os.LookupEnv("HOME")
-    if !ok {
-        fmt.Printf("%s not set\n", "HOME")
-    } else {
-        fmt.Printf("HOME = %s\n", home)
-    }
+    home := getHome()
+    
+    // home, ok := os.LookupEnv("HOME")
+    // if !ok {
+    //     log.Fatalln("$HOME variable not set")
+    //     fmt.Printf("%s not set\n", "HOME")
+    // } 
+
 
     trashHome := fmt.Sprintf("%s/.local/share/TTTTT", home)
     info, err := os.Stat(trashHome)
@@ -22,13 +25,27 @@ func main() {
         fmt.Printf("%s is a directory: %v\n", trashHome, info.IsDir())
     } else {
         // fmt.Printf("error: %v\n", err)
-        err := os.MkdirAll(trashHome, 700)
+        err := os.MkdirAll(trashHome, 0o700)
         if err != nil {
             fmt.Println("failed to create trash directory")
         }
         fmt.Printf("Created trash directory at: %s\n", trashHome)
     }
 
+    // trashFiles := fmt.Sprintf("%s/files", trashHome)
+    // trashInfo := fmt.Sprintf("%s/info", trashHome)
+
 
     
+}
+
+// Function getHome returns the string representation of $HOME.
+// If the HOME variable is not set, a message is logged and the program is exited
+// with a status code of 1.
+func getHome() string {
+    home, ok := os.LookupEnv("HOME")
+    if !ok {
+        log.Fatalln("$HOME variable not set")
+    } 
+    return home
 }
