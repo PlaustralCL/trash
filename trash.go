57c2169ee6880405db0622126569fedeafdc40be
PlaustralCL <plaustralcl@gmail.com>
2026-03-15 12:48:08 -0400

    Remove unused code

--- trash.go
+++ trash.go
@@ -10,31 +10,14 @@ import (
 // Trash location = $XDG_DATA_HOME/Trash
 
 func main() {
-    // home := getHome()
     makeTrashDirectories()
-
-    // trashHome := fmt.Sprintf("%s/.local/share/TTTTT", home)
-    // info, err := os.Stat(trashHome)
-    // if err == nil {
-    //     fmt.Printf("%s is a directory: %v\n", trashHome, info.IsDir())
-    // } else {
-    //     // fmt.Printf("error: %v\n", err)
-    //     err := os.MkdirAll(trashHome, 0o700)
-    //     if err != nil {
-    //         fmt.Println("failed to create trash directory")
-    //     }
-    //     fmt.Printf("Created trash directory at: %s\n", trashHome)
-    // }
-
-    // trashFiles := fmt.Sprintf("%s/files", trashHome)
-    // trashInfo := fmt.Sprintf("%s/info", trashHome)
-
+    fmt.Println("Success!")
 
     
 }
 
 // Function getHome returns the string representation of $HOME.
-// If the HOME variable is not set, a message is logged and the program is exited
+// If the HOME variable is not set, the program is exited
 // with a status code of 1.
 func getHome() string {
     home, ok := os.LookupEnv("HOME")
@@ -55,6 +38,11 @@ func trashPaths() (trashHome, trashFiles, trashInfo string) {
     return trashHome, trashFiles, trashInfo
 }
 
+// Function makeTrashDirectories creates the Trash/, Trash/files, and Trash/info
+// directories if they do not already exist. If the directories already exist,
+// no change is made. If the function is not able to create one of the paths,
+// the program will exit with a status of 1. A value of true is returned
+// if all directories are created successfully.
 func makeTrashDirectories() bool {
     trashHome, trashFiles, trashInfo := trashPaths();
 
@@ -66,17 +54,4 @@ func makeTrashDirectories() bool {
         }
     }
     return true
-
-    // err := os.MkdirAll(trashHome, 0o700)
-    // if err != nil {
-    //     log.Fatalf("Error creating %s\n", trashHome)
-    // }
-
-    // err := os.MkdirAll(trashFiles, 0o700)
-    // if err != nil {
-    //     log.Fatalf("Error creating %s\n", trashFiles)
-    // }
-
-    
-    
 }
