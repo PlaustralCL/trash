a18eeeba9a10a4a22db90b6ebe789f154464dc27
PlaustralCL <plaustralcl@gmail.com>
2026-03-14 20:01:23 -0400

    Create trash directory if needed

--- trash.go
+++ trash.go
@@ -1,10 +1,12 @@
-packge main
+package main
 
 import (
     "fmt"
     "os"
 )
 
+// $XDG_DATA_HOME = $HOME/.local/share
+// Trash location = $XDG_DATA_HOME/Trash
 
 func main() {
     home, ok := os.LookupEnv("HOME")
@@ -13,4 +15,20 @@ func main() {
     } else {
         fmt.Printf("HOME = %s\n", home)
     }
+
+    trashHome := fmt.Sprintf("%s/.local/share/TTTTT", home)
+    info, err := os.Stat(trashHome)
+    if err == nil {
+        fmt.Printf("%s is a directory: %v\n", trashHome, info.IsDir())
+    } else {
+        // fmt.Printf("error: %v\n", err)
+        err := os.MkdirAll(trashHome, 700)
+        if err != nil {
+            fmt.Println("failed to create trash directory")
+        }
+        fmt.Printf("Created trash directory at: %s\n", trashHome)
+    }
+
+
+    
 }
