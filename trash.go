9035d5b00dddf288bfbf0fa53bd9aef138583492
PlaustralCL <plaustralcl@gmail.com>
2026-05-16 14:16:03 -0400

    Clarify usage statement

--- trash.go
+++ trash.go
@@ -15,9 +15,15 @@ func main() {
     flag.Usage = customUsage
     listFlag := flag.Bool("list", false, "List the files in trash")
     pathFlag := flag.Bool("path", false, "Show the the path to the trash directory")
-    restoreFlag := flag.Bool("restore", false, "Restore one or more files")
-    removeFlag := flag.Bool("remove", false, "Permanetly delete one or more files from the trash")
+    restoreFlag := flag.Bool("restore", false, "Select one or more files to retore to their original location.")
+    removeFlag := flag.Bool("remove", false, "Select one or more files to permanetly delete from the trash")
     emptyFlag := flag.Bool("empty", false, "Permanetly delete all files in the trash")
+
+    if len(os.Args) < 2 {
+		flag.Usage()
+		os.Exit(1)
+	}
+    
     flag.Parse()
 
     makeTrashDirectories()
@@ -42,8 +48,10 @@ func main() {
 // Custom usage message
 func customUsage() {
     fmt.Println("Usage of ./trash:")
-    fmt.Println("With no flags set, all arguments are assumed to be files being sent to trash.")
+    fmt.Println("trash <filename> <filename> | [-empty | -list | -path | -remove | restore]")
     fmt.Println("")
+    fmt.Println("With no flags set, all arguments are assumed to be files being sent to trash.")
+    fmt.Println("Filenames of files being sent to the trash should be separated by a space.")    
     fmt.Println("Available flags:")
     flag.PrintDefaults()
 }
